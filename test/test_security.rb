8c74a071ef6b9cce94d76791bba9bbac9af11d61
Daniel Berger <djberg96@gmail.com>
2013-01-05 09:21:21 -0700

    Updated base security test file.

--- test/test_security.rb
+++ test/test_security.rb
@@ -2,33 +2,18 @@
 # test_security.rb
 #
 # Test suite for the Win32::Security base class. You should run these
-# tests via the 'rake test' task.
+# tests via the rake test tasks.
 ########################################################################
-require 'rubygems'
-gem 'test-unit'
-
-require 'test/unit'
+require 'test-unit'
 require 'win32/security'
-require 'windows/system_info'
 
 class TC_Win32_Security < Test::Unit::TestCase
-   extend Windows::SystemInfo
+  test "version constant is set to expected value" do
+    assert_equal('0.2.0', Win32::Security::VERSION)
+  end
 
-   def self.startup
-      @@version = windows_version
-   end
-   
-   def test_version
-      assert_equal('0.1.3', Win32::Security::VERSION)
-   end
-
-   def test_elevated_security
-      omit_if(@@version < 6.0, 'Skipped on Windows 2000 and Windows XP')
-      assert_respond_to(Win32::Security, :elevated_security?)
-      assert_boolean(Win32::Security.elevated_security?)
-   end
-   
-   def self.shutdown
-      @@version= nil
-   end
+  test "elevated security basic functionality" do
+    assert_respond_to(Win32::Security, :elevated_security?)
+    assert_boolean(Win32::Security.elevated_security?)
+  end
 end
