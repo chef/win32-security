24239eb03edc497f4ed125f77f2ecf44563891af
Daniel Berger <djberg96@gmail.com>
2013-01-09 19:26:21 -0700

    Added identifier authority constants.

--- lib/win32/security/windows/constants.rb
+++ lib/win32/security/windows/constants.rb
@@ -4,12 +4,23 @@ module Windows
       TOKEN_QUERY = 8
       ERROR_NO_TOKEN = 1008
 
-      SECURITY_NT_AUTHORITY = 5
+      ACL_REVISION = 2
+      AclSizeInformation = 2
+
       SECURITY_BUILTIN_DOMAIN_RID = 0x00000020
       DOMAIN_ALIAS_RID_ADMINS = 0x00000220
 
-      ACL_REVISION = 2
-      AclSizeInformation = 2
+      # Identifier Authorities
+
+      SECURITY_NULL_SID_AUTHORITY         = 0
+      SECURITY_WORLD_SID_AUTHORITY        = 1
+      SECURITY_LOCAL_SID_AUTHORITY        = 2
+      SECURITY_CREATOR_SID_AUTHORITY      = 3
+      SECURITY_NON_UNIQUE_AUTHORITY       = 4
+      SECURITY_NT_AUTHORITY               = 5
+      SECURITY_RESOURCE_MANAGER_AUTHORITY = 9
+
+      # SID types
 
       SidTypeUser           = 1
       SidTypeGroup          = 2
