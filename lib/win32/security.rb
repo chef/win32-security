9667e492cb7655b010aedc92242e4de263f0c910
Daniel Berger <djberg96@gmail.com>
2013-02-19 18:07:03 -0700

    Version bumps and CHANGES update.

--- lib/win32/security.rb
+++ lib/win32/security.rb
@@ -20,7 +20,7 @@ module Win32
     extend Windows::Security::Functions
 
     # The version of the win32-security library
-    VERSION = '0.2.0'
+    VERSION = '0.2.1'
 
     # Used by OpenProcessToken
     TOKEN_QUERY = 8
