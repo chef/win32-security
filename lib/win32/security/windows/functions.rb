4ed38109babb0ce4a4df3fa31ee23657858487e8
Daniel Berger <djberg96@gmail.com>
2013-04-08 11:33:31 -0600

    Fixed HANDLE prototypes and added some convenient typedefs.

--- lib/win32/security/windows/functions.rb
+++ lib/win32/security/windows/functions.rb
@@ -13,6 +13,10 @@ module Windows
         end
       end
 
+      typedef :ulong, :dword
+      typedef :uintptr_t, :handle
+      typedef :pointer, :ptr
+
       ffi_lib :kernel32
 
       enum :token_info_class, [
@@ -59,39 +63,39 @@ module Windows
         :MaxTokenInfoClass
       ]
 
-      attach_pfunc :GetCurrentProcess, [], :ulong
-      attach_pfunc :GetCurrentThread, [], :ulong
-      attach_pfunc :GetVersionExA, [:pointer], :bool
-      attach_pfunc :GetLastError, [], :ulong
-      attach_pfunc :CloseHandle, [:ulong], :bool
+      attach_pfunc :GetCurrentProcess, [], :handle
+      attach_pfunc :GetCurrentThread, [], :handle
+      attach_pfunc :GetVersionExA, [:ptr], :bool
+      attach_pfunc :GetLastError, [], :dword
+      attach_pfunc :CloseHandle, [:dword], :bool
 
       ffi_lib :advapi32
 
-      attach_pfunc :AddAccessAllowedAce, [:pointer, :ulong, :ulong, :pointer], :bool
+      attach_pfunc :AddAccessAllowedAce, [:ptr, :dword, :dword, :ptr], :bool
       attach_pfunc :AllocateAndInitializeSid,
-        [:pointer, :int, :ulong, :ulong, :ulong, :ulong, :ulong, :ulong, :ulong, :ulong, :pointer], :bool
-      attach_pfunc :CheckTokenMembership, [:ulong, :pointer, :pointer], :bool
-      attach_pfunc :ConvertSidToStringSid, :ConvertSidToStringSidA, [:pointer, :pointer], :bool
-      attach_pfunc :ConvertStringSidToSid, :ConvertStringSidToSidA, [:string, :pointer], :bool
-      attach_pfunc :EqualSid, [:pointer, :pointer], :bool
-      attach_pfunc :FindFirstFreeAce, [:pointer, :pointer], :bool
-      attach_pfunc :GetAclInformation, [:pointer, :pointer, :ulong, :int], :bool
-      attach_pfunc :GetLengthSid, [:pointer], :ulong
-      attach_pfunc :GetSidLengthRequired, [:uint], :ulong
-      attach_pfunc :GetSidSubAuthority, [:pointer, :ulong], :pointer
-      attach_pfunc :GetTokenInformation, [:ulong, :token_info_class, :pointer, :ulong, :pointer], :bool
-      attach_pfunc :InitializeAcl, [:pointer, :ulong, :ulong], :bool
-      attach_pfunc :InitializeSid, [:pointer, :pointer, :uint], :bool
-      attach_pfunc :IsValidAcl, [:pointer], :bool
-      attach_pfunc :IsValidSid, [:pointer], :bool
-      attach_pfunc :IsWellKnownSid, [:pointer, :int], :bool
+        [:ptr, :int, :dword, :dword, :dword, :dword, :dword, :dword, :dword, :dword, :ptr], :bool
+      attach_pfunc :CheckTokenMembership, [:handle, :ptr, :ptr], :bool
+      attach_pfunc :ConvertSidToStringSid, :ConvertSidToStringSidA, [:ptr, :ptr], :bool
+      attach_pfunc :ConvertStringSidToSid, :ConvertStringSidToSidA, [:string, :ptr], :bool
+      attach_pfunc :EqualSid, [:ptr, :ptr], :bool
+      attach_pfunc :FindFirstFreeAce, [:ptr, :ptr], :bool
+      attach_pfunc :GetAclInformation, [:ptr, :ptr, :dword, :int], :bool
+      attach_pfunc :GetLengthSid, [:ptr], :dword
+      attach_pfunc :GetSidLengthRequired, [:uint], :dword
+      attach_pfunc :GetSidSubAuthority, [:ptr, :dword], :ptr
+      attach_pfunc :GetTokenInformation, [:handle, :token_info_class, :ptr, :dword, :ptr], :bool
+      attach_pfunc :InitializeAcl, [:ptr, :dword, :dword], :bool
+      attach_pfunc :InitializeSid, [:ptr, :ptr, :uint], :bool
+      attach_pfunc :IsValidAcl, [:ptr], :bool
+      attach_pfunc :IsValidSid, [:ptr], :bool
+      attach_pfunc :IsWellKnownSid, [:ptr, :int], :bool
       attach_pfunc :LookupAccountName, :LookupAccountNameA,
-        [:string, :string, :pointer, :pointer, :pointer, :pointer, :pointer], :bool
+        [:string, :string, :ptr, :ptr, :ptr, :ptr, :ptr], :bool
       attach_pfunc :LookupAccountSid, :LookupAccountSidA,
-        [:string, :pointer, :pointer, :pointer, :pointer, :pointer, :pointer], :bool
-      attach_pfunc :OpenProcessToken, [:ulong, :ulong, :pointer], :bool
-      attach_pfunc :OpenThreadToken, [:ulong, :ulong, :bool, :pointer], :bool
-      attach_pfunc :SetAclInformation, [:pointer, :pointer, :ulong, :int], :bool
+        [:string, :ptr, :ptr, :ptr, :ptr, :ptr, :ptr], :bool
+      attach_pfunc :OpenProcessToken, [:handle, :dword, :ptr], :bool
+      attach_pfunc :OpenThreadToken, [:handle, :dword, :bool, :ptr], :bool
+      attach_pfunc :SetAclInformation, [:ptr, :ptr, :dword, :int], :bool
     end
   end
 end
