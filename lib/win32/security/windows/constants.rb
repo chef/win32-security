2abbe581f836830cff88f96b40c3736938561cc4
Daniel Berger <djberg96@gmail.com>
2014-10-29 15:15:37 -0600

    Added standard access right constants.

--- lib/win32/security/windows/constants.rb
+++ lib/win32/security/windows/constants.rb
@@ -122,6 +122,26 @@ module Windows
       # SDDL version information
 
       SDDL_REVISION_1 = 1
+
+      # Standard Access Rights
+
+      DELETE                       = 0x00010000
+      READ_CONTROL                 = 0x20000
+      WRITE_DAC                    = 0x40000
+      WRITE_OWNER                  = 0x80000
+      SYNCHRONIZE                  = 0x100000
+      STANDARD_RIGHTS_REQUIRED     = 0xf0000
+      STANDARD_RIGHTS_READ         = 0x20000
+      STANDARD_RIGHTS_WRITE        = 0x20000
+      STANDARD_RIGHTS_EXECUTE      = 0x20000
+      STANDARD_RIGHTS_ALL          = 0x1F0000
+      SPECIFIC_RIGHTS_ALL          = 0xFFFF
+      ACCESS_SYSTEM_SECURITY       = 0x1000000
+      MAXIMUM_ALLOWED              = 0x2000000
+      GENERIC_READ                 = 0x80000000
+      GENERIC_WRITE                = 0x40000000
+      GENERIC_EXECUTE              = 0x20000000
+      GENERIC_ALL                  = 0x10000000
     end
   end
 end
